d08aca63d0c9ec4ed511b4747528da138238d43a
Arialdo Martini <arialdomartini@gmail.com>
2023-08-30 00:40:07 +0200

    Custom notification publisher

---
+++ src/WithoutMediatR/CustomNotificationPublisher/With.cs
@@ -0,0 +1,88 @@
+﻿using Lamar;
+using MediatR;
+using MediatR.NotificationPublishers;
+using Xunit;
+
+namespace WithoutMediatR.CustomNotificationPublisher;
+
+public record PingNotification(string Message) : INotification;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Pong1 : INotificationHandler<PingNotification>
+{
+    internal static string WasInvoked;
+    public Task Handle(PingNotification request, CancellationToken cancellationToken)
+    {
+        // do work
+        WasInvoked = $"Pong1 was notified with {request.Message}";
+        return Task.CompletedTask;
+    }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Pong2 : INotificationHandler<PingNotification>
+{
+    internal static string WasInvoked;
+    public Task Handle(PingNotification request, CancellationToken cancellationToken)
+    {
+        // do work
+        WasInvoked = $"Pong2 was notified with {request.Message}";
+        return Task.CompletedTask;
+    }
+}
+
+
+class CustomNotificationPublisher : INotificationPublisher
+{
+    async Task INotificationPublisher.Publish(IEnumerable<NotificationHandlerExecutor> handlerExecutors, INotification notification, CancellationToken cancellationToken)
+    {
+        foreach (var handler in handlerExecutors)
+        {
+            await handler.HandlerCallback(notification, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
+
+
+
+public class With : IDisposable
+{
+    private readonly Container _container;
+
+    public With()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.Scan(scanner =>
+            {
+                scanner.AssemblyContainingType(typeof(With));
+                scanner.IncludeNamespaceContainingType<PingNotification>();
+                scanner.WithDefaultConventions();
+           
+                scanner.AddAllTypesOf(typeof(INotificationHandler<>));
+            });
+
+            // INotificationPublisher a = new ForeachAwaitPublisher();
+            // INotificationPublisher b = new TaskWhenAllPublisher();
+            INotificationPublisher c = new CustomNotificationPublisher();
+            
+            cfg.For<IMediator>().Add(s => new Mediator(s, c));
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void notification_test()
+    {
+        var mediator = _container.GetInstance<IMediator>();
+
+        await mediator.Publish(new PingNotification(Message: "some message"));
+        
+        Assert.Equal("Pong1 was notified with some message", Pong1.WasInvoked);
+        Assert.Equal("Pong2 was notified with some message", Pong2.WasInvoked);
+    }
+}
---
+++ src/WithoutMediatR/CustomNotificationPublisher/Without.cs
@@ -0,0 +1,90 @@
+﻿using Lamar;
+using MediatR;
+using MediatR.NotificationPublishers;
+using Xunit;
+
+namespace WithoutMediatR.CustomNotificationPublisher.Without;
+
+internal interface IPingNotificationHandler
+{
+    Task NotifyPing(string message);
+}
+
+public class Pong1 : IPingNotificationHandler
+{
+    internal static string WasInvoked;
+    public Task NotifyPing(string message)
+    {
+        // do work
+        WasInvoked = $"Pong1 was notified with {message}";
+        return Task.CompletedTask;
+    }
+}
+
+public class Pong2 : IPingNotificationHandler
+{
+    internal static string WasInvoked;
+    public Task NotifyPing(string message)
+    {
+        // do work
+        WasInvoked = $"Pong2 was notified with {message}";
+        return Task.CompletedTask;
+    }
+}
+
+
+internal interface IPingNotificationPublisher
+{
+    Task Ping(string message);
+}
+
+// ReSharper disable once ClassNeverInstantiated.Local
+file class PingNotificationPublisher : IPingNotificationPublisher
+{
+    private readonly IEnumerable<IPingNotificationHandler> _handlers;
+
+    public PingNotificationPublisher(IEnumerable<IPingNotificationHandler> handlers)
+    {
+        _handlers = handlers;
+    }
+
+    Task IPingNotificationPublisher.Ping(string message)
+    {
+        _handlers.ToList().ForEach(h => h.NotifyPing(message));
+
+        return Task.CompletedTask;
+    }
+}
+
+
+public class Without : IDisposable
+{
+    private readonly Container _container;
+
+    public Without()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.For<IPingNotificationHandler>().Add(new Pong1());
+            cfg.For<IPingNotificationHandler>().Add(new Pong2());
+            
+            cfg.For<IPingNotificationPublisher>().Use<PingNotificationPublisher>();
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void notification_test()
+    {
+        var pingNotificationPublisher = _container.GetInstance<IPingNotificationPublisher>();
+
+        await pingNotificationPublisher.Ping("some message");
+        
+        Assert.Equal("Pong1 was notified with some message", Pong1.WasInvoked);
+        Assert.Equal("Pong2 was notified with some message", Pong2.WasInvoked);
+    }
+}

e0b8b30aa72ad74bc4568d5a6a0950216aa9aa2b
Arialdo Martini <arialdomartini@gmail.com>
2023-08-30 00:08:09 +0200

    Notification

---
+++ src/WithoutMediatR/Notification/With.cs
@@ -0,0 +1,77 @@
+﻿using Lamar;
+using MediatR;
+using Xunit;
+
+namespace WithoutMediatR.Notification;
+
+public class PingNotification : INotification { }
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Pong1 : INotificationHandler<PingNotification>
+{
+    internal static string WasInvoked;
+    public Task Handle(PingNotification request, CancellationToken cancellationToken)
+    {
+        // do work
+        WasInvoked = "Pong 1";
+        return Task.CompletedTask;
+    }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Pong2 : INotificationHandler<PingNotification>
+{
+    internal static string WasInvoked;
+    public Task Handle(PingNotification request, CancellationToken cancellationToken)
+    {
+        // do work
+        WasInvoked = "Pong 2";
+        return Task.CompletedTask;
+    }
+}
+
+public class With : IDisposable
+{
+    private readonly Container _container;
+
+    public With()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.Scan(scanner =>
+            {
+                scanner.AssemblyContainingType(typeof(With));
+                scanner.IncludeNamespaceContainingType<PingNotification>();
+                scanner.WithDefaultConventions();
+           
+                scanner.AddAllTypesOf(typeof(INotificationHandler<>));
+            });
+
+            cfg.For<IMediator>().Use<Mediator>();
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void notification_test()
+    {
+        var mediator = _container.GetInstance<IMediator>();
+
+        await mediator.Publish(new PingNotification());
+        
+        Assert.Equal("Pong 1", Pong1.WasInvoked);
+        Assert.Equal("Pong 2", Pong2.WasInvoked);
+    }
+    
+    [Fact]
+    void sending_a_notifications_fails_at_runtime()
+    {
+        var mediator = _container.GetInstance<IMediator>();
+
+        Assert.ThrowsAsync<Exception>(async () => await mediator.Send(new PingNotification()));
+    }
+}
---
+++ src/WithoutMediatR/Notification/Without.cs
@@ -0,0 +1,88 @@
+﻿using Lamar;
+using Xunit;
+
+namespace WithoutMediatR.Notification.Without;
+
+internal interface IPingNotificationHandler
+{
+    Task NotifyPing();
+}
+
+file class Pong1 : IPingNotificationHandler
+{
+    internal static string WasInvoked;
+
+    Task IPingNotificationHandler.NotifyPing()
+    {
+        // do work
+        WasInvoked = "Pong 1";
+        return Task.CompletedTask;
+    }
+}
+
+file class Pong2 : IPingNotificationHandler
+{
+    internal static string WasInvoked;
+
+    Task IPingNotificationHandler.NotifyPing()
+    {
+        // do work
+        WasInvoked = "Pong 2";
+        return Task.CompletedTask;
+    }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Local
+file class PingNotificationPublisher : IPingNotificationPublisher
+{
+    private readonly IEnumerable<IPingNotificationHandler> _handlers;
+
+    public PingNotificationPublisher(IEnumerable<IPingNotificationHandler> handlers)
+    {
+        _handlers = handlers;
+    }
+
+    Task IPingNotificationPublisher.Ping()
+    {
+        _handlers.ToList().ForEach(h => h.NotifyPing());
+
+        return Task.CompletedTask;
+    }
+}
+
+internal interface IPingNotificationPublisher
+{
+    Task Ping();
+}
+
+public class Without : IDisposable
+{
+    private readonly Container _container;
+
+    public Without()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.For<IPingNotificationHandler>().Add(new Pong1());
+            cfg.For<IPingNotificationHandler>().Add(new Pong2());
+            
+            cfg.For<IPingNotificationPublisher>().Use<PingNotificationPublisher>();
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void notification_test()
+    {
+        var handler = _container.GetInstance<IPingNotificationPublisher>();
+
+        await handler.Ping();
+        
+        Assert.Equal("Pong 1", Pong1.WasInvoked);
+        Assert.Equal("Pong 2", Pong2.WasInvoked);
+    }
+}

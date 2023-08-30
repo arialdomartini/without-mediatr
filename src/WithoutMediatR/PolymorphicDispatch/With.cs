cd4e3acaeb8d73e5cff1dd94d79cd239c9141e97
Arialdo Martini <arialdomartini@gmail.com>
2023-08-30 08:06:35 +0200

    Polymorphic Dispatch

---
+++ src/WithoutMediatR/PolymorphicDispatch/With.cs
@@ -0,0 +1,59 @@
+﻿using Lamar;
+using MediatR;
+using Xunit;
+
+namespace WithoutMediatR.PolymorphicDispatch;
+
+public class FooNotification : INotification { }
+public class BarNotification : INotification { }
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class CatchAll : INotificationHandler<INotification>
+{
+    internal static string WasInvoked;
+    public Task Handle(INotification request, CancellationToken cancellationToken)
+    {
+        // do work
+        WasInvoked = request.GetType().ToString();
+        return Task.CompletedTask;
+    }
+}
+
+public class With : IDisposable
+{
+    private readonly Container _container;
+
+    public With()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.Scan(scanner =>
+            {
+                scanner.AssemblyContainingType(typeof(With));
+                scanner.IncludeNamespaceContainingType<FooNotification>();
+                scanner.WithDefaultConventions();
+           
+                scanner.AddAllTypesOf(typeof(INotificationHandler<>));
+            });
+
+            cfg.For<IMediator>().Use<Mediator>();
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void CatchAll_receives_all_notifications()
+    {
+        var mediator = _container.GetInstance<IMediator>();
+
+        await mediator.Publish(new FooNotification());
+        Assert.Contains("FooNotification", CatchAll.WasInvoked);
+
+        await mediator.Publish(new BarNotification());
+        Assert.Contains("BarNotification", CatchAll.WasInvoked);
+    }
+}
---
+++ src/WithoutMediatR/PolymorphicDispatch/Without.cs
@@ -0,0 +1,57 @@
+﻿using Lamar;
+using MediatR;
+using Xunit;
+
+namespace WithoutMediatR.PolymorphicDispatch;
+
+file record MyNotification { }
+file record FooNotification : MyNotification { }
+file record BarNotification : MyNotification { }
+
+
+file interface INotificationHandler
+{
+    Task NotifyPing(in MyNotification notification);
+}
+
+file class CatchAll : INotificationHandler
+{
+    internal static string WasInvoked;
+
+    Task INotificationHandler.NotifyPing(in MyNotification notification)
+    {
+        // do work
+        WasInvoked = notification.GetType().ToString();
+        return Task.CompletedTask;
+    }
+}
+
+public class Without : IDisposable
+{
+    private readonly Container _container;
+
+    public Without()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.For<INotificationHandler>().Add(new CatchAll());
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void CatchAll_receives_all_notifications()
+    {
+        var handler = _container.GetInstance<INotificationHandler>();
+
+        await handler.NotifyPing(new FooNotification());
+        Assert.Contains("FooNotification", CatchAll.WasInvoked);
+        
+        await handler.NotifyPing(new BarNotification());
+        Assert.Contains("BarNotification", CatchAll.WasInvoked);
+    }
+}

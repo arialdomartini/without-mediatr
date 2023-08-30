5bc0b7ef1d8a2a74c8377419c1da2135a4bb18de
Arialdo Martini <arialdomartini@gmail.com>
2023-08-30 19:50:53 +0200

    Async

---
+++ src/WithoutMediatR/Async/With.cs
@@ -0,0 +1,58 @@
+﻿using Lamar;
+using MediatR;
+using Xunit;
+
+namespace WithoutMediatR.Async;
+
+public class Ping : IRequest<Pong> { }
+public class Pong { }
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class PingHandler : IRequestHandler<Ping, Pong>
+{
+    internal static string WasInvoked;
+    public async Task<Pong> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        WasInvoked = "Pong";
+        return await DoPong();
+    }
+
+    private async Task<Pong> DoPong() => await Task.FromResult(new Pong());
+}
+
+public class With : IDisposable
+{
+    private readonly Container _container;
+
+    public With()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.Scan(scanner =>
+            {
+                scanner.AssemblyContainingType(typeof(With));
+                scanner.IncludeNamespaceContainingType<Ping>();
+                scanner.WithDefaultConventions();
+           
+                scanner.AddAllTypesOf(typeof(IRequestHandler<,>));
+            });
+
+            cfg.For<IMediator>().Use<Mediator>();
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void notification_test()
+    {
+        var mediator = _container.GetInstance<IMediator>();
+
+        await mediator.Send(new Ping());
+        
+        Assert.Equal("Pong", PingHandler.WasInvoked);
+    }
+}
---
+++ src/WithoutMediatR/Async/Without.cs
@@ -0,0 +1,53 @@
+﻿using Lamar;
+using Xunit;
+
+namespace WithoutMediatR.Async;
+
+file class Ping { }
+file class Pong { }
+
+file interface IPingHandler
+{
+    Task<Pong> Handle(Ping request);
+}
+
+file class PingHandler : IPingHandler
+{
+    internal static string WasInvoked;
+
+    async Task<Pong> IPingHandler.Handle(Ping request)
+    {
+        WasInvoked = "Pong";
+        return await DoPong();
+    }
+
+    private async Task<Pong> DoPong() => await Task.FromResult(new Pong());
+}
+
+public class Without : IDisposable
+{
+    private readonly Container _container;
+
+    public Without()
+    {
+        _container = new Container(cfg =>
+        {
+            cfg.For<IPingHandler>().Add(new PingHandler());
+        });
+    }
+
+    void IDisposable.Dispose()
+    {
+        _container.Dispose();
+    }
+
+    [Fact]
+    async void notification_test()
+    {
+        var pingHandler = _container.GetInstance<IPingHandler>();
+
+        await pingHandler.Handle(new Ping());
+        
+        Assert.Equal("Pong", PingHandler.WasInvoked);
+    }
+}

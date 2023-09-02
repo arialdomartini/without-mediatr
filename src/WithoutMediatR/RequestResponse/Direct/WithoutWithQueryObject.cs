76113ed438941858a4b8892d81763aa05a936c43
Arialdo Martini <arialdomartini@gmail.com>
2023-09-02 10:23:44 +0200

    Request/resonse with a query object

---
+++ src/WithoutMediatR/RequestResponse/Direct/WithoutWithQueryObject.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace WithoutMediatR.RequestResponse.Direct;
+
+file class Ping{}
+
+
+file interface IPingHandler
+{
+    string Ping(Ping ping);
+}                           
+
+
+file class PingHandler : IPingHandler
+{
+    string IPingHandler.Ping(Ping ping) => "Pong";
+}
+
+
+public class WithoutWithQueryObject
+{
+    [Fact]
+    void ping_request_response()
+    {
+        IPingHandler pingHandler = new PingHandler();
+        var response = pingHandler.Ping(new Ping());
+        
+        Assert.Equal("Pong", response);
+    }
+}

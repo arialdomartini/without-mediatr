d97d1e168fb8aff82d2dc9b36b81ef0f53fc36eb
Arialdo Martini <arialdomartini@gmail.com>
2023-09-05 23:22:33 +0200

    Request/response with failing subtyping

---
+++ src/WithoutMediatR/RequestResponseSubtyping/Direct/With.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace WithoutMediatR.RequestResponseSubtyping.Direct;
+
+file record SubTypeOfPing : Ping;
+file record Ping : IRequest<string>;
+
+file class PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("Pong");
+    }
+}
+
+public class With
+{
+    private readonly Mediator _mediator;
+
+    public With()
+    {
+        var serviceProvider =
+            new ServiceCollection()
+                .AddTransient<IRequestHandler<Ping, string>, PingHandler>()
+                .BuildServiceProvider();
+
+        _mediator = new Mediator(serviceProvider);
+    }
+
+    [Fact]
+    async Task Ping_is_delivered()
+    {
+        var response = await _mediator.Send(new Ping());
+        
+        Assert.Equal("Pong", response);
+    }
+    
+    [Fact]
+    async Task SubTypeOfPing_is_not_delivered()
+    {
+        await Assert.ThrowsAsync(
+            typeof(InvalidOperationException), 
+            async () => await _mediator.Send(new SubTypeOfPing())); 
+    }
+}
